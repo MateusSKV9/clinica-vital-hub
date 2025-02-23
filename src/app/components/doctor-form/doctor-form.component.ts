f058e46b770c7b3503d682f84f2d151f671d479b
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-23 20:44:41 -0300

    feat: create doctor-form component

---
+++ src/app/components/doctor-form/doctor-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctor-form.component.html',
+  styleUrl: './doctor-form.component.css',
+})
+export class DoctorFormComponent {}
