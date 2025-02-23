26448bfcefed65cbcdff8ba6f4ee833047e405ab
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-23 20:45:44 -0300

    feat: create exam-form component

---
+++ src/app/components/exam-form/exam-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exam-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './exam-form.component.html',
+  styleUrl: './exam-form.component.css',
+})
+export class ExamFormComponent {}
