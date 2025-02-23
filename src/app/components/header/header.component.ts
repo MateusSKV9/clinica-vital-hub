fbb6b77dfc6c044191a2458842e8e417d6dabd90
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-23 20:55:40 -0300

    feat(header.component): add basic structure and dropdowns

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  dropdownStates: { [key: number]: boolean } = {};
+
+  showDropDown(index: number): void {
+    this.dropdownStates[index] = true;
+  }
+
+  hideDropDown(index: number): void {
+    this.dropdownStates[index] = false;
+  }
+}
