78234a297a1df9493ddb42a1e92f894889b83501
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-23 20:46:17 -0300

    feat: create material-form component

---
+++ src/app/components/material-form/material-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-material-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './material-form.component.html',
+  styleUrl: './material-form.component.css',
+})
+export class MaterialFormComponent {}
