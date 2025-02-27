efc695987a3b00df7f85e1c845f080cc9cf648a8
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-27 11:12:02 -0300

    feat(appointment-form.component): add and style basic structure

--- src/app/components/appointment-form/appointment-form.component.ts
+++ src/app/components/appointment-form/appointment-form.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ClearButtonComponent } from "../shared/clear-button/clear-button.component";
+import { RegisterButtonComponent } from "../shared/register-button/register-button.component";
 
 @Component({
   selector: 'app-appointment-form',
   standalone: true,
-  imports: [],
+  imports: [ClearButtonComponent, RegisterButtonComponent],
   templateUrl: './appointment-form.component.html',
   styleUrl: './appointment-form.component.css',
 })
