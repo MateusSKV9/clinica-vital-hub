4bd97684700720f1da9eceb1a7b2589c6621f37c
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-27 11:11:32 -0300

    feat(exam-form.component): add and style basic structure

--- src/app/components/exam-form/exam-form.component.ts
+++ src/app/components/exam-form/exam-form.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ClearButtonComponent } from "../shared/clear-button/clear-button.component";
+import { RegisterButtonComponent } from "../shared/register-button/register-button.component";
 
 @Component({
   selector: 'app-exam-form',
   standalone: true,
-  imports: [],
+  imports: [ClearButtonComponent, RegisterButtonComponent],
   templateUrl: './exam-form.component.html',
   styleUrl: './exam-form.component.css',
 })
