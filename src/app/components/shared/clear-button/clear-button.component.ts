2e4642ce9cf4b60dea49feb07a140856f9ad9a6a
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-26 21:23:03 -0300

    feat: create and style form buttons

---
+++ src/app/components/shared/clear-button/clear-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clear-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './clear-button.component.html',
+  styleUrl: './clear-button.component.css',
+})
+export class ClearButtonComponent {}
