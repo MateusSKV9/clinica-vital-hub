9aae400df006e0540f4f2f560e465c49a6b3ed5e
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-26 21:25:50 -0300

    feat: add form buttons to patient and doctor form components

--- src/app/components/doctor-form/doctor-form.component.ts
+++ src/app/components/doctor-form/doctor-form.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AddressFormComponent } from "../shared/address-form/address-form.component";
+import { ClearButtonComponent } from "../shared/clear-button/clear-button.component";
+import { RegisterButtonComponent } from "../shared/register-button/register-button.component";
 
 @Component({
   selector: 'app-doctor-form',
   standalone: true,
-  imports: [AddressFormComponent],
+  imports: [AddressFormComponent, ClearButtonComponent, RegisterButtonComponent],
   templateUrl: './doctor-form.component.html',
   styleUrl: './doctor-form.component.css',
 })
--- src/app/components/patient-form/patient-form.component.ts
+++ src/app/components/patient-form/patient-form.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AddressFormComponent } from "../shared/address-form/address-form.component";
+import { RegisterButtonComponent } from "../shared/register-button/register-button.component";
+import { ClearButtonComponent } from "../shared/clear-button/clear-button.component";
 
 @Component({
   selector: 'app-patient-form',
   standalone: true,
-  imports: [AddressFormComponent],
+  imports: [AddressFormComponent, RegisterButtonComponent, ClearButtonComponent],
   templateUrl: './patient-form.component.html',
   styleUrl: './patient-form.component.css',
 })
