19232fb3cd348d70967812a8e7ea1a8bdcc61c9b
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-27 13:54:21 -0300

    feat(routes.ts): add schedule routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { DoctorFormComponent } from './components/doctor-form/doctor-form.compon
 import { MaterialFormComponent } from './components/material-form/material-form.component';
 import { ExamFormComponent } from './components/exam-form/exam-form.component';
 import { AppointmentFormComponent } from './components/appointment-form/appointment-form.component';
+import { AppointmentSchedulingComponent } from './components/appointment-scheduling/appointment-scheduling.component';
+import { ExamSchedulingComponent } from './components/exam-scheduling/exam-scheduling.component';
+import { ScheduleViewComponent } from './components/schedule-view/schedule-view.component';
+import { ScheduleSearchComponent } from './components/schedule-search/schedule-search.component';
+import { CancellationReschedulingComponent } from './components/cancellation-rescheduling/cancellation-rescheduling.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,5 +23,14 @@ export const routes: Routes = [
       { path: 'consulta', component: AppointmentFormComponent },
     ],
   },
-  
+  {
+    path: 'agendamento',
+    children: [
+      { path: 'consulta', component: AppointmentSchedulingComponent },
+      { path: 'exame', component: ExamSchedulingComponent },
+      { path: 'visualizacao_agenda', component: ScheduleViewComponent },
+      { path: 'pesquisa_horarios', component: ScheduleSearchComponent },
+      { path: 'cancelamento_reagendamento', component: CancellationReschedulingComponent },
+    ],
+  },
 ];
