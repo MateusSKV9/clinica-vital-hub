044ef83e9b613c989f1e956a717f13f9532a4c6b
Mateus Santos <mateussilvanzg@gmail.com>
2025-02-26 21:38:00 -0300

    feat(material-form.component): add and style basic structure

--- src/app/components/material-form/material-form.component.ts
+++ src/app/components/material-form/material-form.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ClearButtonComponent } from "../shared/clear-button/clear-button.component";
+import { RegisterButtonComponent } from "../shared/register-button/register-button.component";
 
 @Component({
   selector: 'app-material-form',
   standalone: true,
-  imports: [],
+  imports: [ClearButtonComponent, RegisterButtonComponent],
   templateUrl: './material-form.component.html',
   styleUrl: './material-form.component.css',
 })
