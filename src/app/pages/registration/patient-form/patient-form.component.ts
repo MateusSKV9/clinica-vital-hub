41027e0413578b55663df549fca0307ba21817dd
Mateus Santos <mateussilvanzg@gmail.com>
2025-03-06 22:34:27 -0300

    feat(patient-form.component): implement form validation

--- src/app/pages/registration/patient-form/patient-form.component.ts
+++ src/app/pages/registration/patient-form/patient-form.component.ts
@@ -8,11 +8,15 @@ import {
   Validators,
   ReactiveFormsModule,
 } from '@angular/forms';
+import { PatientService } from '../../../services/patient.service';
+import { Patient } from '../../../interfaces/patient.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-patient-form',
   standalone: true,
   imports: [
+    CommonModule,
     AddressFormComponent,
     RegisterButtonComponent,
     ClearButtonComponent,
@@ -28,20 +32,54 @@ export class PatientFormComponent implements OnInit {
     this.initializeForm();
   }
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private patientService: PatientService
+  ) {}
 
   initializeForm(): void {
     this.form = this.fb.group({
-      name: ['', [Validators.required]],
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.pattern(/^[a-zA-Z\s]+$/),
+        ],
+      ],
       dateOfBirth: ['', [Validators.required]],
       bloodType: ['', [Validators.required]],
       phone: ['', [Validators.required]],
       healthInsurance: ['', [Validators.required]],
-      height: ['', [Validators.required]],
-      weight: ['', [Validators.required]],
+      height: ['', [Validators.required, Validators.min(1)]],
+      weight: ['', [Validators.required, Validators.min(1)]],
       medicalHistory: ['', [Validators.required]],
+
+      formAddress: this.fb.group({
+        street: ['', Validators.required],
+        number: ['', Validators.required],
+        neighborhood: ['', Validators.required],
+        complement: [''],
+        city: ['', Validators.required],
+        zipCode: ['', Validators.required],
+        state: ['', Validators.required],
+      }),
     });
   }
 
-  onSubmit() {}
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    const patient = this.form.value as Patient;
+    this.patientService
+      .create(patient)
+      .subscribe(() => console.log('Paciente cadastrado!'));
+  }
+
+  get formAddress(): FormGroup {
+    return this.form.get('formAddress') as FormGroup;
+  }
 }
