c8d2dfcb01380018368f4b85a3394b26765024db
Mateus Santos <mateussilvanzg@gmail.com>
2025-03-06 22:28:19 -0300

    feat(address.component): implement form validation

--- src/app/components/shared/address-form/address-form.component.ts
+++ src/app/components/shared/address-form/address-form.component.ts
@@ -1,36 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-address-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [NgxMaskDirective, ReactiveFormsModule, CommonModule],
   templateUrl: './address-form.component.html',
   styleUrl: './address-form.component.css',
 })
-export class AddressFormComponent implements OnInit {
-  form!: FormGroup;
-
-  ngOnInit(): void {}
-
-  constructor(private fb: FormBuilder) {}
-
-  initializeForm(): void {
-    this.form = this.fb.group({
-      street: ['', [Validators.required]],
-      number: ['', [Validators.required]],
-      neighborhood: ['', [Validators.required]],
-      complement: ['', [Validators.required]],
-      city: ['', [Validators.required]],
-      zidCode: ['', [Validators.required]],
-      state: ['', [Validators.required]],
-    });
-  }
-
-  onSubmit() {}
+export class AddressFormComponent {
+  @Input() formAddress!: FormGroup;
 }
