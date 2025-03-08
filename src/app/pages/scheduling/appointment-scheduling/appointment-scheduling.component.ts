1db4bb8ff14f99de2f2c42ce92bb6cbb7d9fda6d
Mateus Santos <mateussilvanzg@gmail.com>
2025-03-08 11:13:48 -0300

    feat(appointment-scheduling.component): implement form validation

--- src/app/pages/scheduling/appointment-scheduling/appointment-scheduling.component.ts
+++ src/app/pages/scheduling/appointment-scheduling/appointment-scheduling.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClearButtonComponent } from '../../../components/shared/clear-button/clear-button.component';
 import { RegisterButtonComponent } from '../../../components/shared/register-button/register-button.component';
+import { CommonModule } from '@angular/common';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-appointment-scheduling',
   standalone: true,
-  imports: [ClearButtonComponent, RegisterButtonComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ClearButtonComponent,
+    RegisterButtonComponent,
+  ],
   templateUrl: './appointment-scheduling.component.html',
   styleUrl: './appointment-scheduling.component.css',
 })
-export class AppointmentSchedulingComponent {}
+export class AppointmentSchedulingComponent implements OnInit {
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      doctor: ['', [Validators.required]],
+      patient: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      time: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+  }
+}
