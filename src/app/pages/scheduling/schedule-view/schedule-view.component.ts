854a6bf8eaebc2c9eb634a0f1cef2e6c7cee35e2
Mateus Santos <mateussilvanzg@gmail.com>
2025-03-08 11:16:01 -0300

    feat(schedule-view.component): implement form validation

--- src/app/pages/scheduling/schedule-view/schedule-view.component.ts
+++ src/app/pages/scheduling/schedule-view/schedule-view.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClearButtonComponent } from '../../../components/shared/clear-button/clear-button.component';
 import { RegisterButtonComponent } from '../../../components/shared/register-button/register-button.component';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-schedule-view',
   standalone: true,
   imports: [
+    CommonModule,
+    ReactiveFormsModule,
     ClearButtonComponent,
     RegisterButtonComponent,
     ClearButtonComponent,
@@ -14,4 +23,26 @@ import { RegisterButtonComponent } from '../../../components/shared/register-but
   templateUrl: './schedule-view.component.html',
   styleUrl: './schedule-view.component.css',
 })
-export class ScheduleViewComponent {}
+export class ScheduleViewComponent implements OnInit {
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      doctor: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+  }
+}
