6b6416ef2cefb77a66c39e620a42326ab2cfe411
Mateus Santos <mateussilvanzg@gmail.com>
2025-03-11 19:26:05 -0300

    feat(app.routes): implement lazy loading and add redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,52 +1,129 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AppointmentFormComponent } from './pages/registration/appointment-form/appointment-form.component';
-import { DoctorFormComponent } from './pages/registration/doctor-form/doctor-form.component';
-import { ExamFormComponent } from './pages/registration/exam-form/exam-form.component';
-import { MaterialFormComponent } from './pages/registration/material-form/material-form.component';
-import { PatientFormComponent } from './pages/registration/patient-form/patient-form.component';
-import { ConsultationReportComponent } from './pages/reports/consultation-report/consultation-report.component';
-import { ExaminationReportComponent } from './pages/reports/examination-report/examination-report.component';
-import { FinancialReportComponent } from './pages/reports/financial-report/financial-report.component';
-import { MaterialsReportComponent } from './pages/reports/materials-report/materials-report.component';
-import { AppointmentSchedulingComponent } from './pages/scheduling/appointment-scheduling/appointment-scheduling.component';
-import { CancellationReschedulingComponent } from './pages/scheduling/cancellation-rescheduling/cancellation-rescheduling.component';
-import { ExamSchedulingComponent } from './pages/scheduling/exam-scheduling/exam-scheduling.component';
-import { ScheduleSearchComponent } from './pages/scheduling/schedule-search/schedule-search.component';
-import { ScheduleViewComponent } from './pages/scheduling/schedule-view/schedule-view.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+
+  // ################# REGISTRATION ################# //
+
   {
     path: 'cadastro',
     children: [
-      { path: 'paciente', component: PatientFormComponent },
-      { path: 'medico', component: DoctorFormComponent },
-      { path: 'material', component: MaterialFormComponent },
-      { path: 'exame', component: ExamFormComponent },
-      { path: 'consulta', component: AppointmentFormComponent },
-    ],
-  },
-  {
-    path: 'agendamento',
-    children: [
-      { path: 'consulta', component: AppointmentSchedulingComponent },
-      { path: 'exame', component: ExamSchedulingComponent },
-      { path: 'visualizacao-agenda', component: ScheduleViewComponent },
-      { path: 'pesquisa-horarios', component: ScheduleSearchComponent },
       {
-        path: 'cancelamento-reagendamento',
-        component: CancellationReschedulingComponent,
+        path: 'paciente',
+        loadComponent: () =>
+          import(
+            './pages/registration/patient-form/patient-form.component'
+          ).then((m) => m.PatientFormComponent),
+      },
+      {
+        path: 'medico',
+        loadComponent: () =>
+          import('./pages/registration/doctor-form/doctor-form.component').then(
+            (m) => m.DoctorFormComponent
+          ),
+      },
+      {
+        path: 'material',
+        loadComponent: () =>
+          import(
+            './pages/registration/material-form/material-form.component'
+          ).then((m) => m.MaterialFormComponent),
+      },
+      {
+        path: 'exame',
+        loadComponent: () =>
+          import('./pages/registration/exam-form/exam-form.component').then(
+            (m) => m.ExamFormComponent
+          ),
+      },
+      {
+        path: 'consulta',
+        loadComponent: () =>
+          import(
+            './pages/registration/appointment-form/appointment-form.component'
+          ).then((m) => m.AppointmentFormComponent),
       },
     ],
   },
+
+  // ################# SCHEDULING ################# //
+
+  {
+    path: 'agendamento',
+    children: [
+      {
+        path: 'consulta',
+        loadComponent: () =>
+          import(
+            './pages/scheduling/appointment-scheduling/appointment-scheduling.component'
+          ).then((m) => m.AppointmentSchedulingComponent),
+      },
+      {
+        path: 'exame',
+        loadComponent: () =>
+          import(
+            './pages/scheduling/exam-scheduling/exam-scheduling.component'
+          ).then((m) => m.ExamSchedulingComponent),
+      },
+      {
+        path: 'visualizacao-agenda',
+        loadComponent: () =>
+          import(
+            './pages/scheduling/schedule-view/schedule-view.component'
+          ).then((m) => m.ScheduleViewComponent),
+      },
+      {
+        path: 'pesquisa-horarios',
+        loadComponent: () =>
+          import(
+            './pages/scheduling/schedule-search/schedule-search.component'
+          ).then((m) => m.ScheduleSearchComponent),
+      },
+      {
+        path: 'cancelamento-reagendamento',
+        loadComponent: () =>
+          import(
+            './pages/scheduling/cancellation-rescheduling/cancellation-rescheduling.component'
+          ).then((m) => m.CancellationReschedulingComponent),
+      },
+    ],
+  },
+
+  // ################# REPORTS ################# //
+
   {
     path: 'relatorio',
     children: [
-      { path: 'consulta', component: ConsultationReportComponent },
-      { path: 'exame', component: ExaminationReportComponent },
-      { path: 'financeiro', component: FinancialReportComponent },
-      { path: 'materiais', component: MaterialsReportComponent },
+      {
+        path: 'consulta',
+        loadComponent: () =>
+          import(
+            './pages/reports/consultation-report/consultation-report.component'
+          ).then((m) => m.ConsultationReportComponent),
+      },
+      {
+        path: 'exame',
+        loadComponent: () =>
+          import(
+            './pages/reports/examination-report/examination-report.component'
+          ).then((m) => m.ExaminationReportComponent),
+      },
+      {
+        path: 'financeiro',
+        loadComponent: () =>
+          import(
+            './pages/reports/financial-report/financial-report.component'
+          ).then((m) => m.FinancialReportComponent),
+      },
+      {
+        path: 'materiais',
+        loadComponent: () =>
+          import(
+            './pages/reports/materials-report/materials-report.component'
+          ).then((m) => m.MaterialsReportComponent),
+      },
     ],
   },
+  { path: '**', redirectTo: '' },
 ];
