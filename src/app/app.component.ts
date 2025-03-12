6bf8d9401a83d4c43fa71f7c9f65d417eb9f9a09
Mateus Santos <mateussilvanzg@gmail.com>
2025-03-11 22:01:27 -0300

    feat(app.component): add spinnner for page loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { RouterOutlet } from '@angular/router';
-import { NgxSpinnerComponent } from 'ngx-spinner';
+import {
+  NavigationEnd,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { NgxSpinnerComponent, NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
@@ -16,5 +21,19 @@ import { NgxSpinnerComponent } from 'ngx-spinner';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  constructor(
+    private router: Router,
+    private spinnerService: NgxSpinnerService
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.spinnerService.show();
+      }
+      if (event instanceof NavigationEnd) {
+        this.spinnerService.hide();
+      }
+    });
+  }
+
   title = 'clinica-vital-hub';
 }
