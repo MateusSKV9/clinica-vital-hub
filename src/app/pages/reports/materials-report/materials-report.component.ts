2a3420c6e8d1d2663c0878300ea394274429ba97
Mateus Santos <mateussilvanzg@gmail.com>
2025-03-12 21:08:42 -0300

    feat(materials-report):  implement material listing

--- src/app/pages/reports/materials-report/materials-report.component.ts
+++ src/app/pages/reports/materials-report/materials-report.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ClearButtonComponent } from '../../../components/shared/clear-button/clear-button.component';
 import { RegisterButtonComponent } from '../../../components/shared/register-button/register-button.component';
+import { MaterialService } from '../../../services/material/material.service';
+import { Material } from '../../../interfaces/Material.interface';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-materials-report',
   standalone: true,
-  imports: [RegisterButtonComponent, ClearButtonComponent],
+  imports: [RegisterButtonComponent, ClearButtonComponent, ReactiveFormsModule],
   templateUrl: './materials-report.component.html',
   styleUrl: './materials-report.component.css',
 })
-export class MaterialsReportComponent {}
+export class MaterialsReportComponent implements OnInit {
+  form!: FormGroup;
+  materials!: Material[];
+
+  constructor(
+    private materialService: MaterialService,
+    private fb: FormBuilder
+  ) {
+    this.getMaterials();
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      period: ['', [Validators.required]],
+      materialType: ['', [Validators.required]],
+      format: [''],
+    });
+  }
+
+  onSubmit() {}
+
+  getMaterials(): void {
+    this.materialService
+      .getAll()
+      .subscribe((materials) => (this.materials = materials));
+  }
+  
+}
